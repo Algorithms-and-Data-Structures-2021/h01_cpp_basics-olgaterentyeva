9ee93f16bad571b08db7d142111defe7f843731b
Olga <terenteva2002@yandex.ru>
2021-03-14 14:00:08 +0300

    Author and Book done, Book_Store failed

--- src/book_store.cpp
+++ src/book_store.cpp
@@ -5,37 +5,57 @@
 
 // 1. реализуйте функцию ...
 ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
-  // здесь мог бы быть ваш разносторонний и многогранный код ...
-  // Tip 1: проведите валидацию аргументов функции
-  // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
-  return ResizeStorageStatus::SUCCESS;
+// здесь мог бы быть ваш разносторонний и многогранный код ...
+// Tip 1: проведите валидацию аргументов функции
+    if (storage == nullptr) {
+        return ResizeStorageStatus::NULL_STORAGE;
+    }
+    if (new_capacity <= size) {
+        return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
+    }
+    if (size < 0) {
+        return ResizeStorageStatus::NEGATIVE_SIZE;
+    }
+//Tip 2: не забудьте высвободить ранее выделенную память под хранилище
+    Book *newStorage = new Book[new_capacity];
+    for (int i = 0; i < size; ++i) {
+        newStorage[i] = storage[i];
+    }
+    delete storage;
+    return ResizeStorageStatus::SUCCESS;
 }
 
 // 2. реализуйте конструктор ...
 BookStore::BookStore(const std::string &name) : name_{name} {
-  // валидация аргумента
-  if (name.empty()) {
-    throw std::invalid_argument("BookStore::name must not be empty");
-  }
+// валидация аргумента
+    if (name.empty()) {
+        throw std::invalid_argument("BookStore::name must not be empty");
+    }
+// здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
+    this -> name_ = name;
+    this -> name_ = storage_capacity_;
 
-  // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
+    Book bookStore[kInitStorageCapacity];
 }
 
 // 3. реализуйте деструктор ...
 BookStore::~BookStore() {
-  // здесь мог бы быть ваш высвобождающий разум от негатива код ...
-  // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+// здесь мог бы быть ваш высвобождающий разум от негатива код ...
+// Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+    delete[] storage_;
 }
 
 // 4. реализуйте метод ...
 void BookStore::AddBook(const Book &book) {
-  if (storage_size_ == storage_capacity_) {
-    // здесь мог бы быть ваш умопомрачительный код ...
-    // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
-    // Tip 2: не забудьте обработать статус вызова функции
-  }
-  // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
+    if (storage_size_ == storage_capacity_) {
+// Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
+// Tip 2: не забудьте обработать статус вызова функции
+        if (resize_storage_internal(kCapacityCoefficient) != ResizeStorageStatus::SUCCESS) return;
+    }
+// Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
+    storage_[storage_size_] = book;
 }
+
 
 // РЕАЛИЗОВАНО
 
